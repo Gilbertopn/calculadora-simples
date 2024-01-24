345b7c343add3e153826e236da32c4408f34fe43
Gilberto Junior <juninhossj4@gmail.com>
2024-01-24 00:54:10 -0300

    fazendo uma concatenação simples com o nome

---
+++ src/main/java/org/example/Concatenacao.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Concatenacao {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String firstName = "Gilberto";
+        String seconodName = "Junior";
+
+        System.out.println("Meu nome é " + firstName + " e meu sobrenome é " + seconodName + ".");
+    }
+}
